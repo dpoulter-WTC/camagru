fe54548610cad2a56a9167f72254358eeb5d9635
Coding <coding@Gabbys-MacBook-Air.local>
2018-11-05 11:01:02 +0200

    merging

--- photo_upload.php
+++ photo_upload.php
@@ -6,14 +6,11 @@ $data = str_replace(" ", "+", $data);
 list($type, $data) = explode(';', $data);
 list(, $data)      = explode(',', $data);
 $data = base64_decode($data);
-echo 'hi';
-echo $_SESSION['curr_user'];
 $counter = 0;
-while (file_exists($_SESSION['curr_user'] . $counter))
+while (file_exists($_SERVER['DOCUMENT_ROOT'] . "/photos/".$_SESSION['curr_user'] . $counter .'.jpeg'))
 {
   $counter++;
 }
-file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/photos/".$_SESSION['curr_user'].$counter++.'.jpeg', $data);
-
+file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/photos/".$_SESSION['curr_user'].$counter.'.jpeg', $data);
 die;
 ?>
