c727c39d10bf79d826d6864af99bf6a287c0c0ed
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-11-07 13:46:55 +0200

    Pushing updated photo_edit and sticker name

--- photo_edit.php
+++ photo_edit.php
@@ -3,8 +3,20 @@
 <body>
 
 <p>Image to use:</p>
-<img id="scream" src="resources/profile/profile.jpg" alt="The Scream">
-<img id="sticker" width="100" height="100" src="resources/stickers/09.png" alt="The Scream">
+<img id="original" src="resources/profile/profile.jpg" alt="Original Image">
+<img id="sticker1" width="100" height="100" src="resources/stickers/BuyMeStuff.png" alt="Sticker 1">
+<img id="sticker2" width="100" height="100" src="resources/stickers/BlackCat.png" alt="Sticker 2">
+<img id="sticker3" width="100" height="100" src="resources/stickers/ColourSkull.png" alt="Sticker 3">
+<img id="sticker4" width="100" height="100" src="resources/stickers/CrystalWolf.png" alt="Sticker 4">
+<img id="sticker5" width="100" height="100" src="resources/stickers/DabbingSquidward.png" alt="Sticker 5">
+<img id="sticker6" width="100" height="100" src="resources/stickers/DontTouch.png" alt="Sticker 6">
+<img id="sticker7" width="100" height="100" src="resources/stickers/Elephant.png" alt="Sticker 7">
+<img id="sticker8" width="100" height="100" src="resources/stickers/Ew.png" alt="Sticker 8">
+<img id="sticker9" width="100" height="100" src="resources/stickers/FlowerYinYang.png" alt="Sticker 9">
+<img id="sticker10" width="100" height="100" src="resources/stickers/OhNoTigger.png" alt="Sticker 10">
+<img id="sticker11" width="100" height="100" src="resources/stickers/RainbowSloth.png" alt="Sticker 11">
+<img id="sticker12" width="100" height="100" src="resources/stickers/UnluckyCat.png" alt="Sticker 12">
+<img id="sticker13" width="100" height="100" src="resources/stickers/WatercolourFlower.png" alt="Sticker 13">
 
 <p>Canvas:</p>
 <canvas id="myCanvas" width="240" height="297" style="border:1px solid #d3d3d3;">
@@ -15,10 +27,10 @@ Your browser does not support the HTML5 canvas tag.
 window.onload = function() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    var img = document.getElementById("scream");
-    var sticker = document.getElementById("sticker");
+    var img = document.getElementById("original");
+    var sticker = document.getElementById("sticker1");
     ctx.drawImage(img, 1, 1);
-    addSticker(ctx, sticker);
+    addSticker(ctx, sticker, 'topLeft');
 }
 function addSticker(ctx, sticker, position)
 {
