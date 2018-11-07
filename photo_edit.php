aeaeb26b3997ec9939df7d66b0206881f46a4f8d
Meghan Westvig <mwestvig@student.wethinkcode.co.za>
2018-11-07 13:23:30 +0200

    Adding edited stickers and edited photo_edit.php

--- photo_edit.php
+++ photo_edit.php
@@ -3,8 +3,8 @@
 <body>
 
 <p>Image to use:</p>
-<img id="scream" width="220" height="277" src="resources/profile/profile.jpg" alt="The Scream">
-<img id="sticker" width="220" height="277" src="resources/stickers/09.png" alt="The Scream">
+<img id="scream" src="resources/profile/profile.jpg" alt="The Scream">
+<img id="sticker" width="100" height="100" src="resources/stickers/09.png" alt="The Scream">
 
 <p>Canvas:</p>
 <canvas id="myCanvas" width="240" height="297" style="border:1px solid #d3d3d3;">
@@ -17,13 +17,36 @@ window.onload = function() {
     var ctx = c.getContext("2d");
     var img = document.getElementById("scream");
     var sticker = document.getElementById("sticker");
-    ctx.drawImage(img, 10, 10);
-    ctx.drawImage(sticker, 10, 10);
+    ctx.drawImage(img, 1, 1);
+    addSticker(ctx, sticker);
+}
+function addSticker(ctx, sticker, position)
+{
+  var x;
+  var y;
+  if (position == 'topLeft')
+  {
+      x = 10;
+      y = 10;
+  }
+  else if (position == 'topRight')
+  {
+
+  }
+  else if (position == 'bottomLeft')
+  {
+
+  }
+  else if (position == 'bottomRight')
+  {
+
+  }
+  else if (position == 'centre')
+  {
+
+  }
+  ctx.drawImage(sticker, x, y, 50, 50);
 }
 </script>
-
-<p><strong>Note:</strong> The canvas tag is not supported in Internet
-Explorer 8 and earlier versions.</p>
-
 </body>
 </html>
